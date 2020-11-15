7a669f88d42cff978ff3af52b9a56c00658df613
wakana12345 <http.www.wakana@gmail.com>
2020-11-15 15:17:19 +0900

    add four file

---
+++ setup1.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ubuntu/repos/RaspberryPiMouse/src/drivers/ 
+/sbin/insmod rtmouse.ko
--- setup2.bash
+++ setup2.bash
@@ -4,8 +4,3 @@ exec 2> /tmp/setup.log
 
 cd /home/ubuntu/repos/RaspberryPiMouse/src/drivers/
 /sbin/insmod rtmouse.ko
-
-sleep 1
-chmod 666 /dev/rt*
-
-echo 0 > /dev/rtmotoren0
